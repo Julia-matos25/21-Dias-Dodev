9e125409e9b05197969a66300516361b007e241f
Julia Matos <juliaandrade10matos@gmail.com>
2023-09-12 16:08:19 -0300

    Corrigindo

--- Dia13/ex2.js
+++ Dia13/ex2.js
@@ -1,23 +1,60 @@
-class Carro {
-    constructor(nome, potencia, velocidadeMaxima, aceleracao) {
-        this.nome = nome;
-        this.potencia = potencia;
-        this.velocidadeMaxima = velocidadeMaxima;
-        this.aceleracao = aceleracao;
-    }
+class Carro 
+{
+  Nome
+  Potencia
+  VelocidadeMaxima
+  Aceleracao
 
-    calcularTempoParaPercorrerDistancia(distanciaEmMetros) {
-        if (this.velocidadeMaxima <= 0 || this.aceleracao <= 0) {
-            throw new Error("A velocidade máxima e a aceleração devem ser maiores que zero.");
-        }
+  constructor(nome, potencia, velocidadeMaxima, aceleracao) {
+      this.Nome = nome
+      this.Potencia = potencia
+      this.VelocidadeMaxima = velocidadeMaxima
+      this.Aceleracao = aceleracao
+  }
 
-        const tempo = distanciaEmMetros / (this.velocidadeMaxima / this.aceleracao);
-        return tempo;
-    }
+  CalcularTempoMedio(distancia) {
+      let resultado = distancia / (this.VelocidadeMaxima / this.Aceleracao)
+      return resultado
+  }
 }
 
-// Exemplo de uso
-const carroFerrari = new Carro("Ferrari", 700, 320, 3.5);
-const distanciaAPercorrer = 1000; // em metros
-const tempoParaPercorrer = carroFerrari.calcularTempoParaPercorrerDistancia(distanciaAPercorrer);
-console.log(`Tempo para percorrer ${distanciaAPercorrer} metros: ${tempoParaPercorrer.toFixed(2)} segundos`);
+class Corrida
+{
+  Nome
+  Tipo 
+  Distancia 
+  Participantes 
+  Vencedor
+
+  constructor(nome, tipo, distancia){
+    this.Nome = nome
+    this.Tipo = tipo
+    this.Distancia = distancia
+    this.Vencedor = ""
+    this.Participantes = []
+  }
+DefinirVencedor(){
+    let menorTempo = this.Participantes[0].CalcularTempoMedio(this.Distancia)
+    let vencedor = this.Participantes[0]
+    for(let index = 1; index < this.Participantes.length; index++){
+        let tempo = this.Participantes[index].CalcularTempoMedio(this.Distancia)
+        if(tempo < menorTempo){
+           menorTempo = tempo
+           vencedor = this.Participantes[index]
+        }
+        
+    }
+    return this.Vencedor = vencedor
+ }  
+  ExibirVencedor(){
+    alert("O vencedor é: " + this.Vencedor.Nome)
+  }
+}
+let corrida = new Corrida("Monza", "Formula 1", 6000)
+
+corrida.Participantes[0] = new Carro("Kicks", 120, 160, 5)
+corrida.Participantes[1] = new Carro("Marea", 210, 200, 9)
+corrida.Participantes[2] = new Carro("Peugeot 206", 300, 220, 10)
+
+corrida.DefinirVencedor()
+corrida.ExibirVencedor()
